f25846975a4875b2be13c84630dc63f95370dbf3
CSBlues <laforests@adhevilleschool.org>
2020-09-02 14:48:01 -0400

    Initial commit

---
+++ Strings.playground/Pages/Defining Strings.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+/*:
+## Defining Strings
+In the previous playground, you declared number constants like this:
+ */
+let heightInCentimeters = 7817
+//: To declare a string constant, you put text between quotation marks like this:
+let favoriteBook = "The Sun Also Rises"
+
+let traditionalGreeting = "Hello, world!"
+/*:
+The declared string values also appear in the results sidebar. 👉
+
+ - Experiment: Practice by declaring `favoriteMovie` and `favoriteSong` string constants for your favorite movie and song:
+ */
+// Declare a favoriteMovie constant
+
+// Declare a favoriteSong constant
+
+let myNumber = 15
+let stringInterpolation = "My number is: \(favoriteBook)"
+let myString = "This is my string. \n \"This is a quote\"."
+
+let strConcat = "string 1 " + "string 2" + favoriteBook
+
+/*:
+Next, take a string-based trip around the world.
+
+[Previous](@previous)  |  page 3 of 18  |  [Next: Unicode](@next)
+ */
